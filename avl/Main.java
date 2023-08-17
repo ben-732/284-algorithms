22b64e71d010fb475e2fcce8fff2213900285c00
Ben <22252563+ben-732@users.noreply.github.com>
2023-08-17 18:50:05 +1200

    Test Script

--- avl/Main.java
+++ avl/Main.java
@@ -2,33 +2,21 @@ public class Main {
   public static void main(String[] args) {
     Avl thing = new Avl();
 
+    int n = 1000000;
 
-
-    for (int i = 1; i <= 128; i++) {
+    // Add 1 million numbers
+    for (int i = 0; i < n + 10; i++) {
       thing.add(i);
     }
 
-    thing.remove(6);
-    thing.remove(7);
-    thing.remove(13);
-    thing.remove(33);
-    thing.remove(124);
-    thing.remove(22);
-    thing.remove(93);
+    for (int j = 0; j < n; j++) {
+      thing.find(j);
+    }
+
+    for (int k = 0; k < n; k++) {
+      thing.remove(k);
+    }
 
     thing.print();
-
-
-    // Add 1 million numbers
-    // for (int i = 0; i < 1000000; i++) {
-    // thing.add(i);
-    // }
-
-    // for (int j = 0; j < 1000000; j++) {
-    // thing.find(345353);
-    // }
-
-
-
   }
 }
